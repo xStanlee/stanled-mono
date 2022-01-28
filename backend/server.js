e26ee1cdb0e546d53c682b90fbd5099171c2885f
Patryk Stachura <external.patryk.stachura@kuehne-nagel.com>
2022-01-28 22:22:25 +0100

    Reconstruction of app

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/stanled-frontend', 'index.html'));
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
